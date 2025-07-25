b9b1e4a391f7f3a07ea85efbf8174d02627cd3d7
Gabriel Pereira Alves <157809837+GabrielPereiraAl@users.noreply.github.com>
2025-07-25 01:42:52 +0000

    Desafio 2 - Densidade Populacional e PIP per Capital

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,9 @@ int main() {
     char nome_cidade1[30];
     int populacao1;
     float area1;
+    float densidade_pop1;
     float pib1;
+    float pib_per_capital1;
     int pontos_turisticos1;
 
     // Variáveis da segunda cidade
@@ -16,7 +18,9 @@ int main() {
     char nome_cidade2[30];
     int populacao2;
     float area2;
+    float densidade_pop2;
     float pib2;
+    float pib_per_capital2;
     int pontos_turisticos2;
 
     // Cadastro da carta 1
@@ -37,11 +41,19 @@ int main() {
     printf("Digite o tamanho da área da cidade (em km²): ");
     scanf("%f", &area1);
 
-    printf("Digite o valor do PIB da cidade: ");
+    printf("Digite o valor do PIB da cidade: R$ ");
     scanf("%f", &pib1);
 
     printf("Digite a quantidade de pontos turísticos da cidade: ");
     scanf("%d", &pontos_turisticos1);
+    
+    //Calculos de densidade populacional e PIP per capital - Cidade 1
+    
+    //Para a densidade populacional, é calculado (População / área)
+    densidade_pop1 = (float) (populacao1 / area1 );
+    //Para mostrar o PIP per capital, é feito calculo (PIP / população x 1bilhão)
+    pib_per_capital1 = (float) (pib1 / populacao1 * 1000000000);
+
 
     // Apresentação dos dados da carta 1
     printf("\n\n==================== Carta 1 ====================\n");
@@ -49,9 +61,11 @@ int main() {
     printf("Código: %c%s\n", estado1, codigo_carta1);
     printf("Nome da cidade: %s\n", nome_cidade1);
     printf("População: %d\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
+    printf("Área: %.2f km²\n", area1); 
     printf("PIB: R$ %.2f bilhões\n", pib1);
     printf("Pontos turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade populacional: %.2f hab/km² \n", densidade_pop1);
+    printf("PIP per capital: R$ %.2f \n", pib_per_capital1);
     printf("=================================================");
 
     // Cadastro da carta 2
@@ -72,11 +86,19 @@ int main() {
     printf("Digite o tamanho da área da cidade (em km²): ");
     scanf("%f", &area2);
 
-    printf("Digite o valor do PIB da cidade: ");
+    printf("Digite o valor do PIB da cidade: R$ ");
     scanf("%f", &pib2);
 
     printf("Digite a quantidade de pontos turísticos da cidade: ");
     scanf("%d", &pontos_turisticos2);
+
+    //Calculos de densidade populacional e PIP per capital - Cidade 2
+
+    //Para a densidade populacional, é calculado (População / área)
+    densidade_pop2 = (float) (populacao2 / area2 );
+    //Para mostrar o PIP per capital, é feito calculo (PIP / população x 1bilhão)
+    pib_per_capital2 = (float) (pib2 / populacao2 * 1000000000);
+
 
     // Apresentação dos dados da carta 2
     printf("\n\n==================== Carta 2 ====================\n");
@@ -87,6 +109,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: R$ %.2f bilhões\n", pib2);
     printf("Pontos turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade populacional: %.2f hab/km² \n", densidade_pop2);
+    printf("PIP per capital: R$ %.2f \n", pib_per_capital2);
     printf("=================================================\n");
 
     return 0;
